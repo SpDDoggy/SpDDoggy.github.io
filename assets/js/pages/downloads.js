a34e505b6ad3c388907f69665c75e2c439313004
SpringDoogy <274231135@qq.com>
2026-07-30 12:59:57 +0800

    Publish GDB Previewer downloads

--- assets/js/pages/downloads.js
+++ assets/js/pages/downloads.js
@@ -1,4 +1,4 @@
-import { initializeDisplayPreferences } from "../components/display-preferences.js?v=20260730-3";
+import { initializeDisplayPreferences } from "../components/display-preferences.js?v=20260730-4";
 
 initializeDisplayPreferences();
 
--- assets/js/pages/site.js
+++ assets/js/pages/site.js
@@ -1,3 +1,3 @@
-import { initializeDisplayPreferences } from "../components/display-preferences.js?v=20260730-3";
+import { initializeDisplayPreferences } from "../components/display-preferences.js?v=20260730-4";
 
 initializeDisplayPreferences();
